a4a9168263a4dbfd414cc32eee06b0f0d0c54540
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-30 17:27:48 +0100

    feat: implement convertToCelsius function
    This commit adds the implementation of the "convertToCelsius" function,
    which converts a temperature from Fahrenheit to Celsius.
    This feature was implemented based on the specifications
    and passed all the necessary tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const roundNumber = (number) => {
+  return Math.round(number * 10) / 10;
+};
+const convertToCelsius = function (temp) {
+  const celsius = ((temp - 32) * 5) / 9;
+  return roundNumber(celsius);
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function () {};
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
