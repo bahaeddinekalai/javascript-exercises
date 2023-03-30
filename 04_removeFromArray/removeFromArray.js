98f5f5c89c0b5a40da8b84f7374457b46a7c7ed6
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-30 06:01:25 +0100

    chore: make sure that the function work as intended

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function (myArray, ...args) {
   for (let arg of args) {
-    if (typeof arg == "number" && myArray.indexOf(arg) != -1) {
+    if (myArray.indexOf(arg) != -1) {
       myArray.splice(myArray.indexOf(arg), 1);
     }
   }
