875eb4ee083160fad68ffe137c2211ceeaa47207
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-30 04:24:01 +0100

    feat: add code to pass the first TDD test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (textToRepeat, numberOfRepetition) {
+  let returnString = "";
+  for (let i = 0; i < numberOfRepetition; i++) {
+    returnString += textToRepeat;
+  }
+  return returnString;
 };
 
 // Do not edit below this line
