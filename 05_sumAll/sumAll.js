a02a611eadd79d6b69ad93f83ede5ebad4b5fa47
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-30 16:08:20 +0100

    feat: add smallFirst function
    To make sure that the function can work with the large number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function (firstNumber, secondeNumber) {
+const smallFirst = (firstNumber = 1, secondeNumber = 4) => {
+  if (firstNumber > secondeNumber) {
+    firstNumber = firstNumber + secondeNumber;
+    secondeNumber = firstNumber - secondeNumber;
+    firstNumber = firstNumber - secondeNumber;
+  }
+  return [firstNumber, secondeNumber];
+};
+const sumAll = function (firstNumber = 1, secondeNumber = 4) {
   let result = 0;
+  [firstNumber, secondeNumber] = smallFirst(firstNumber, secondeNumber);
   for (let i = firstNumber; i < secondeNumber + 1; i++) {
     result += i;
   }
