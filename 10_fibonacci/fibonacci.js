0b4efe0a8ca5d51ce0404e5e4fdcd9b9f152fe0a
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-31 01:36:39 +0100

    feat: Implement the fibonacci function
    This commit adds the fibonacci function to the codebase,
    which takes a number n as input and returns the nth number
    in the Fibonacci sequence as output.
    The function has been implemented and tested to ensure it works correctly.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  number = parseInt(number);
+  if (number < 1) return "OOPS";
+  if (number == 1 || number == 2) return 1;
+  let firstNumber = 1;
+  let secondeNumber = 1;
+  let result;
+  for (let i = 3; i <= number; i++) {
+    result = secondeNumber + firstNumber;
+    secondeNumber = firstNumber;
+    firstNumber = result;
+  }
+  return result;
 };
-
+console.log(fibonacci(3));
 // Do not edit below this line
 module.exports = fibonacci;
