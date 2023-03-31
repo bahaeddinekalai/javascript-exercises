1f31fb05a418dd2276b1eb12204293e6e77f0cdd
bahaeddinekalai <bahaeddinekalai@gmail.com>
2023-03-31 01:12:51 +0100

    feat: Implement the palindromes function
    This commit adds the palindromes function to the codebase,
    which takes a string as input and returns true if
    the string is a palindrome and false otherwise.
    The function has been implemented and tested to ensure it works correctly.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const onlyAToZ = (str) => {
+  const notInAZ = [" ", "!", ",", "."];
+  for (let notIn of notInAZ) {
+    str = str.replaceAll(notIn, "");
+  }
+  return str;
+};
+const palindromes = function (str) {
+  str = str.toUpperCase();
+  str = onlyAToZ(str);
 
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] != str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+  //console.log(str[0]);
 };
 
 // Do not edit below this line
